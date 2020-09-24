1646a8f5d8058ccb4ca6d9ba70aaeeabaf67eb22
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-09-24 12:38:10 +0530

    Update host-setup.sh

--- host-setup.sh
+++ host-setup.sh
@@ -38,7 +38,7 @@ tar xvf openshift-origin-client-tools*.tar.gz
 cd openshift-origin-client*/
 mv  oc kubectl  /usr/local/bin/
 cd
-rm -rf get-docker.sh openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+rm -rf get-docker.sh openshift-origin-client-tools-* 
 
 public_ip=`ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
 oc cluster up --routing-suffix="${public_ip}.nip.io"
