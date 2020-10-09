976abe9d1156465108b89b0ac87580b5da2c4fdc
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-10-09 17:36:30 +0530

    Update ocp-setup.sh

--- ocp-setup.sh
+++ ocp-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # OCP host setup script for CentOS
 
-yum install -y yum-utils device-mapper-persistent-data lvm2 git curl wget bind-utils jq httpd-tools zip unzip nfs-utils nmap telnet dos2unix java-1.8.0-openjdk
+yum install -q -y yum-utils device-mapper-persistent-data lvm2 git curl wget bind-utils jq httpd-tools zip unzip nfs-utils nmap telnet dos2unix java-1.8.0-openjdk
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
@@ -33,7 +33,7 @@ firewall-cmd --permanent --zone dockerc --add-port={80,443,8443}/tcp
 firewall-cmd --permanent --zone dockerc --add-port={53,8053}/udp
 firewall-cmd --reload
 
-wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+wget -q https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
 tar xvf openshift-origin-client-tools*.tar.gz
 cd openshift-origin-client*/
 mv  oc kubectl  /usr/local/bin/
@@ -46,12 +46,12 @@ oc cluster up --routing-suffix="${public_ip}.nip.io"
 oc cluster status
 
 # Deploying dynamic NFS based persistant storage
-wget https://raw.githubusercontent.com/cloudcafetech/ocpsetup/master/nfsstorage-setup.sh
+wget -q https://raw.githubusercontent.com/cloudcafetech/ocpsetup/master/nfsstorage-setup.sh
 chmod +x ./nfsstorage-setup.sh
 #./nfsstorage-setup.sh
 
 # Kafka setup
-wget https://raw.githubusercontent.com/cloudcafetech/kafka-on-container/master/kafka-setup.sh
+wget -q https://raw.githubusercontent.com/cloudcafetech/kafka-on-container/master/kafka-setup.sh
 chmod +x ./kafka-setup.sh
 #./kafka-setup.sh
 
